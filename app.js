3f282265e4374aaeb4ff068d4887e35cc387b1bc
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-08 14:21:00 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+// Add an if test to Exercise 30 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+let userNames = ["admin", "Usman", "Umar", "Ahsan", "Abdullah"];
+if (userNames.length === 0) {
+    console.log(" We need to find some users!");
+}
+else {
+    userNames = [];
+    console.log("All the users have been removed. " + userNames.length);
+}
